e1eef3a993044f341c1ef8fcf1074d0ff004c904
Ricardo Ruiz Diaz <ricardo.ruiz.diaz@hotmail.es>
2020-03-02 09:45:12 +0100

    CO-0001
    - Insertado la Practica0

---
+++ Practica 0/natural.pl
@@ -0,0 +1,18 @@
+%-------------------------------------------
+% natural(+Numero)
+% es ciertto si Numero es un numero natural
+%-------------------------------------------
+
+/* Comentarios :
+
+    1) natural(1)
+
+    2) natutal(n-1) -> natural(n) (como implicacion lógica, o regla logica)
+    natural(N) :- N>1, N2 is N - 1, natural(N2).
+
+    % a /\ b /\ c /\ -> d
+    d :- a, b, c.
+*/
+
+natural(1).
+natural(N) :-  N>1, N2 is N - 1, natural(N2).
